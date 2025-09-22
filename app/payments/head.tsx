9bfd60a125d0451e8b05125b21f7654e0aad0880
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-22 14:17:25 -0400

    Defer Stripe checkout load with lazy SDK

---
+++ app/payments/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="preconnect" href="https://stripe.com" crossOrigin="anonymous" />
+      <link rel="preconnect" href="https://js.stripe.com" crossOrigin="anonymous" />
+    </>
+  )
+}
